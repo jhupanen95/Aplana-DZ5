e71f1d0c2deb2169f4072a19c8fc599f0b87eb0f
Vladimir <jhupanen@bk.ru>
2019-11-11 17:49:56 +0300

    delete imports

--- src/main/java/GetRequest.java
+++ src/main/java/GetRequest.java
@@ -1,15 +1,10 @@
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.stream.Stream;
 
 public class GetRequest {
 
